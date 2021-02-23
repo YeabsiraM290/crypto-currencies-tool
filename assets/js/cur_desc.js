82cdee941f351cb70bfac52bb7bbc5c1e8bc7751
ermias mulugeta <ermiasmulu19@gmail.com>
2021-02-23 20:12:52 +0300

    creating the variables for the functions

---
+++ assets/js/cur_desc.js
@@ -0,0 +1,27 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+const date_table=document.querySelector('table')
+
+const bitcoin_img=document.createElement('img')
+const description=document.getElementById('description_content')
+const bitcoin_heading=document.createElement('h1')
+const coin_date_=document.getElementById('date_input');
+
+const coin_desc1=document.createElement('p')
+const coin_desc2=document.createElement('a')
+const coin_desc3=document.createElement('p')
+const coin_desc4=document.createElement('p')
+const coin_desc5=document.createElement('p')
+//the next div
+const coin_desc6=document.createElement('p')
+const coin_desc7=document.createElement('p')
+const coin_desc8=document.createElement('p')
+const coin_desc9=document.createElement('p')
+const coin_desc10=document.createElement('p')
+
+
+const contentdiv_1=document.getElementById('left_label_coin_desc')
+const contentdiv_2=document.getElementById('right_label_coin_desc')
+const curr_desc_img=document.getElementById('midea_2_id')
+// 
+const coin_id="ethereum"
+// 
