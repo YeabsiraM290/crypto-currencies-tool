c20aac6dd2c5a5bf0a040d40bc79cfe92c008f41
Natnael <it.natnael.zerihun@gmail.com>
2021-02-28 09:55:13 -0500

    defined a function to fill the container with platform \data

--- assets/js/platforms.js
+++ assets/js/platforms.js
@@ -11,3 +11,31 @@ async function getPlatforms(){
 
     return data;
 }
+function fillPlatforms(){
+
+    getPlatforms().then(function(platforms) {
+        let outPut="";
+        platforms.forEach(function(platform) {
+
+            outPut+=`
+            <a href="${platform.website_url}">
+            <div class="row platRow mb-1">
+
+            <div class="col-4 mt-4"><b>Name</b>: ${platform.name}</div>
+            
+            <div class="col-4 mt-4"><b>Centeralized</b>: ${platform.centralized}</div>
+             
+            <div class="col-4 mt-4"><b>Category</b>: ${platform.category}</div>
+
+            </div></a>`
+    
+            });
+            container.innerHTML = outPut;
+        })
+        .catch(function(err) {
+
+            console.log(err)
+
+        });
+}
+        
