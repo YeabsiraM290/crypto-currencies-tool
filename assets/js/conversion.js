2603ec4f9e82aa498fe1ace92ec315eb564f0b36
Tekletsadik A <it.tekletsadik.abebayehutirfe@gmail.com>
2021-02-28 12:17:41 -0700

    added more functionality on our conversion page

--- assets/js/conversion.js
+++ assets/js/conversion.js
@@ -26,3 +26,61 @@ async function getCoinList(){
     return data;
 
 }
+
+async function getConverted(converted, convertedTo){
+
+    let response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=${converted}&vs_currencies=${convertedTo}`);
+
+    let data = await response.json();
+
+    return data;
+
+}
+
+async function getTrending(converted, convertedTo){
+
+    let response = await fetch('https://api.coingecko.com/api/v3/search/trending');
+
+    let data = await response.json();
+
+    return data;
+
+}
+
+function setTrending(){
+
+    getTrending().then(function(coinList) {
+        let outPut = "";
+
+        for(i=0;i<7;i++){
+
+            let result = Object.values(Object.values(Object.values(coinList)[0][i]))[0]
+            outPut+=`<div class="row Tcoin mt-4">
+            <div class="col-12">
+            <div class="card">
+            <img class="card-img-top" src="${result.large}" height=300 width=300 alt="">
+            <div class="card-body">
+              <h1 class="card-title text-center">${result.name}</h1>
+              <p class="card-text">
+               <b>Symbol:</b> ${result.symbol} <br>
+               <b>Score:</b> ${result.score} <br>
+               <b>Market Cap:</b> ${result.market_cap_rank}
+              </p>
+            </div>
+           </div></div>
+      
+        </div>`
+
+        }
+
+            trendingCoins.innerHTML = outPut;
+
+        })
+
+        .catch(function(err) {
+
+            console.log(err)
+
+        });
+
+}
